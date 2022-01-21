2bf94d373e82fa80369bb30d7d977936fe847f33
Kay Illner <kay.illner@gmail.com>
2022-01-21 12:07:02 +0100

    test a simple stick with verlet constraints

--- index.js
+++ index.js
@@ -5,17 +5,34 @@ let height = canvas.height;
 let aspect = height / width;
 
 const points = [];
+const sticks = [];
 
 const bounceDamping = 0.93;
 const gravity = 0.3;
 const friction = 0.994;
 
+function distance(p0, p1) {
+  const dX = p1.x - p0.x;
+  const dY = p1.y - p0.y;
+
+  return Math.sqrt(dX * dX + dY * dY);
+}
+
 function update() {
   updatePoints();
+  updateSticks();
+
+  constrainBorders();
+
+  context.clearRect(0, 0, width, height);
+
+  renderSticks();
   renderPoints();
+
   requestAnimationFrame(update);
 }
 
+// applies physics to the points
 function updatePoints() {
   points.forEach((p) => {
     vX = (p.x - p.prevX) * friction;
@@ -33,7 +50,33 @@ function updatePoints() {
     // MIND: gravity is an accelerating force, so it should alter velocity to be strict
     // this will result in the same though since the change in position will result in reduced gravity in next update
     p.y += gravity;
+  });
+}
 
+// constraints point position with the sticks
+function updateSticks() {
+  sticks.forEach((s) => {
+    const dX = s.p1.x - s.p0.x;
+    const dY = s.p1.y - s.p0.y;
+
+    const currentLength = distance(s.p0, s.p1);
+    const dLength = currentLength - s.length;
+
+    const relativeLengthChange = dLength / currentLength;
+
+    const offsetX = dX * 0.5 * relativeLengthChange;
+    const offsetY = dY * 0.5 * relativeLengthChange;
+
+    s.p0.x += offsetX;
+    s.p0.y += offsetY;
+
+    s.p1.x -= offsetX;
+    s.p1.y -= offsetY;
+  });
+}
+
+function constrainBorders() {
+  points.forEach((p) => {
     // bounce at borders
     if (p.x > width) {
       p.x = width;
@@ -47,12 +90,21 @@ function updatePoints() {
 }
 
 function renderPoints() {
-  context.clearRect(0, 0, width, height);
   points.forEach((p) => {
     context.beginPath();
     context.arc(p.x, p.y, 2, 0, Math.PI * 2);
     context.fill();
   });
+}
+
+function renderSticks() {
+  context.beginPath();
+  sticks.forEach((s) => {
+    context.moveTo(s.p0.x, s.p0.y);
+    context.lineTo(s.p1.x, s.p1.y);
+  });
+
+  context.stroke();
 }
 
 window.onload = function () {
@@ -61,6 +113,18 @@ window.onload = function () {
     y: 95,
     prevX: 14,
     prevY: 94,
+  });
+  points.push({
+    x: 120,
+    y: 80,
+    prevX: 120,
+    prevY: 80,
+  });
+
+  sticks.push({
+    p0: points[0],
+    p1: points[1],
+    length: distance(points[0], points[1]),
   });
 
   // start render loop
